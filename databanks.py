14840c6187910b23ac9d75bf5034b7941e85571c
luap42 / Paul <pstrobach15+luap42@gmail.com>
2021-04-06 15:06:57 +0200

    Some small fixes

--- databanks.py
+++ databanks.py
@@ -5,6 +5,7 @@ from .config import SETTINGS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = SETTINGS['SQL_URL']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 migrate = Migrate(app, db)
