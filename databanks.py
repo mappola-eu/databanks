3e7c807d888eab20adb158f84b7c802088a1e3d1
luap42 / Paul <pstrobach15+luap42@gmail.com>
2021-06-14 16:17:06 +0200

    Add flask-security/implement simple auth

--- databanks.py
+++ databanks.py
@@ -1,6 +1,7 @@
 from flask import *
 from flask_migrate import Migrate
-from .models import db
+from flask_security import Security, SQLAlchemyUserDatastore
+from .models import db, Role, User
 from .config import SETTINGS
 
 from . import controllers
@@ -8,10 +9,15 @@ from . import controllers
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = SETTINGS['SQL_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'mappola-databanks#secret_key'
+app.config['SECURITY_PASSWORD_SALT'] = 'mappola-databanks#salt'
 
 db.init_app(app)
 migrate = Migrate(app, db)
 app.db = db
+
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
 
 @app.route("/")
 def index():
--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,4 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 from .inscriptions import *                # noqa: E402, F401, F403
+from .users import *                       # noqa: E402, F401, F403
