b101a80aa50330c5279ee7c42a09bf33f7e84421
luap42 / Paul <pstrobach15+luap42@gmail.com>
2021-07-22 16:56:39 +0200

    Allow renaming enumerations

--- controllers/enum.py
+++ controllers/enum.py
@@ -4,6 +4,7 @@ from flask_security import login_required
 
 enum = Blueprint('enum', __name__)
 
+@login_required
 @enum.route("/<name>")
 def show(name):
     try:
@@ -11,3 +12,18 @@ def show(name):
     except: abort(403)
 
     return render_template("enum/show.html", name=name, enum=enum)
+
+@login_required
+@enum.route("/<name>/edit/<id>", methods=["GET", "POST"])
+def edit(name, id):
+    try:
+        enum = get_enum(name)
+    except: abort(403)
+
+    item = enum.query.get_or_404(id)
+
+    if request.method == "POST":
+        item.title = request.form['title']
+        db.session.commit()
+
+    return render_template("enum/edit.html", name=name, enum=enum, item=item)
