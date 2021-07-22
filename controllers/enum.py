4eada61af57be089aba0d252d99aafea1a1cc93a
luap42 / Paul <pstrobach15+luap42@gmail.com>
2021-07-22 17:15:36 +0200

    Implement live editing of enums

--- controllers/enum.py
+++ controllers/enum.py
@@ -27,3 +27,33 @@ def edit(name, id):
         db.session.commit()
 
     return render_template("enum/edit.html", name=name, enum=enum, item=item)
+
+@login_required
+@enum.route("/<name>/add", methods=["GET", "POST"])
+def new(name):
+    try:
+        enum = get_enum(name)
+    except: abort(403)
+
+    item = enum()
+    item.title = ""
+
+    if request.method == "POST":
+        item.title = request.form['title']
+        db.session.add(item)
+        db.session.commit()
+
+        return redirect(url_for('enum.show', name=name))
+
+    return render_template("enum/new.html", name=name, enum=enum, item=item)
+
+@login_required
+@enum.route("/<name>/api")
+def api(name):
+    try:
+        enum = get_enum(name)
+    except: abort(403)
+
+    data = [(i.id, i.title) for i in enum.query.all()]
+
+    return {"data": data}
--- static/js/enumedit.js
+++ static/js/enumedit.js
@@ -1,16 +1,47 @@
 document.querySelectorAll(".modal-toggle").forEach( (mt) => {
-    mt.addEventListener("click", (e) => {
-        document.querySelector(e.target.getAttribute("data-modal")).classList.toggle("__visible");
+    mt.addEventListener("click", async (e) => {
+        const MODAL = document.querySelector(e.target.getAttribute("data-modal"));
+        
+        if(MODAL.hasAttribute("data-enum-key")) {
+            const key = MODAL.getAttribute("data-enum-key");
+            const list = document.querySelector(MODAL.getAttribute("data-enum-list"));
+
+            json_data = await fetch(key);
+            data = await json_data.json();
+
+            current_values = list.value;
+            list.innerHTML = "";
+
+            for (const item of data.data) {
+                el = document.createElement("option")
+                el.value = item[0];
+                el.innerText = item[1];
+                list.appendChild(el);
+            }
+
+            list.value = current_values;
+        }
+        
+        MODAL.classList.toggle("__visible");
         e.preventDefault();
     } );
 } );
 
 document.querySelectorAll(".edit-enum-link").forEach( (mt) => {
-    mt.addEventListener("click", (e) => {
-        document.querySelector(e.target.getAttribute("data-modal")).querySelector("iframe").setAttribute("src", e.target.getAttribute("href"));
+    mt.addEventListener("click", async (e) => {
+        const BASE_URL = e.target.getAttribute("href");
+        const MODAL = document.querySelector(e.target.getAttribute("data-modal"));
+
+        MODAL.querySelector("iframe").setAttribute("src", BASE_URL);
         setTimeout(() => {
-            document.querySelector(e.target.getAttribute("data-modal")).classList.toggle("__visible");
+            MODAL.classList.toggle("__visible");
         }, 150);
+
+        console.log(e.target.previousSibling);
+        console.log(e.target);
+
+        MODAL.setAttribute("data-enum-list", '#' + e.target.parentNode.querySelector("select").getAttribute("id"));
+        MODAL.setAttribute("data-enum-key", BASE_URL + '/api');
         e.preventDefault();
     } );
 } );
