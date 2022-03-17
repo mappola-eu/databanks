571213cbe8de4ccf7e6a9861777d060ce5c2c7e1
luap42 / Paul <pstrobach15+luap42@gmail.com>
2022-03-18 00:54:54 +0100

    Fixes and New RelRes page

--- controllers/resource.py
+++ controllers/resource.py
@@ -22,7 +22,10 @@ def index(name):
     except:
         abort(403)
 
-    return render_template("resource/index.html", name=name, R=R, defn=get_defn(name, scope="summary"))
+    return render_template("resource/index.html",
+                           name=name,
+                           R=R,
+                           defn=get_defn(name, scope="summary"))
 
 
 @resource.route("/<name>/show/<id>", methods=["GET", "POST"])
@@ -34,7 +37,11 @@ def show(name, id):
 
     item = R.query.get_or_404(id)
 
-    return render_template("resource/show.html", name=name, R=R, item=item, defn=get_defn(name, scope="display"))
+    return render_template("resource/show.html",
+                           name=name,
+                           R=R,
+                           item=item,
+                           defn=get_defn(name, scope="display"))
 
 
 @login_required
@@ -49,48 +56,21 @@ def edit(name, id):
     defn = get_defn(name, scope="display")
 
     if request.method == "POST":
-        for slide in defn['slides']:
-            for part in slide['parts']:
-                if part["component"] == "standalone":
-                    cols = [part['single']]
-                elif part["component"] == "table":
-                    cols = part['columns']
-                else:
-                    continue # for now
-
-                for column in cols:
-                    if column['type'] in ['input', 'text', 'reference', 'reference_list']:
-                        # These are the simple (singular) column types
-                        if column['column'] not in request.form.keys():
-                            continue
-
-                        if column['type'] in ['input', 'text']:
-                            setattr(item, column['column'], request.form[column['column']])
-                        elif column['type'] == 'reference':
-                            cls = get_enum(column['refersto'])
-                            obj = cls.query.get(request.form[column['column']])
-                            setattr(item, column['column'], obj)
-                        elif column['type'] == 'reference_list':
-                            cls = get_enum(column['refersto'])
-                            data = [cls.query.get(i) for i in request.form.getlist(column['column'])]
-                            setattr(item, column['column'], data)
-                    elif column['type'] in ['dimension']:
-                        # Special Dimension type
-                        for colname in column['column']:
-                            value = request.form[colname].strip()
-                            if value == '':
-                                value = None
-                            setattr(item, colname, value)
-                    else:
-                        print("LOG: unsaveable column %s" % column['column'])
+        item = apply_defn_post_data_to_obj(defn, item)
 
         db.session.commit()
         flash('Changes committed successfully.')
         return redirect(url_for('resource.edit', name=name, id=item.id))
 
-    return render_template("resource/edit.html", name=name, R=R, item=item,
-                           defn=defn, defn_parse=defn_parse, render_column=render_column,
-                           defn_parse_raw=defn_parse_raw, get_enum=get_enum)
+    return render_template("resource/edit.html",
+                           name=name,
+                           R=R,
+                           item=item,
+                           defn=defn,
+                           defn_parse=defn_parse,
+                           render_column=render_column,
+                           defn_parse_raw=defn_parse_raw,
+                           get_enum=get_enum)
 
 
 @login_required
@@ -101,13 +81,17 @@ def delete(name, id):
     except:
         abort(403)
 
-    item = enum.query.get_or_404(id)
+    item = R.query.get_or_404(id)
 
     if request.method == "POST":
         db.session.delete(item)
         db.session.commit()
 
-    return render_template("resource/delete.html", name=name, R=R, item=item, defn=get_defn(name, scope="display"))
+    return render_template("resource/delete.html",
+                           name=name,
+                           R=R,
+                           item=item,
+                           defn=get_defn(name, scope="display"))
 
 
 @login_required
@@ -123,13 +107,22 @@ def new(name):
     item.title = ""
 
     if request.method == "POST":
-        pass
+        item = apply_defn_post_data_to_obj(defn, item)
+        db.session.add(item)
+        db.commit()
 
         return redirect(url_for('resource.index', name=name))
 
-    return render_template("resource/new.html", name=name, R=R, item=item,
-                           defn=defn, defn_parse=defn_parse, render_column=render_column,
-                           defn_parse_raw=defn_parse_raw, get_enum=get_enum)
+    return render_template("resource/new.html",
+                           name=name,
+                           R=R,
+                           item=item,
+                           defn=defn,
+                           defn_parse=defn_parse,
+                           render_column=render_column,
+                           defn_parse_raw=defn_parse_raw,
+                           get_enum=get_enum)
+
 
 @login_required
 @resource.route("/<name>/-/<id>/rel/<relname>")
@@ -143,27 +136,105 @@ def relindex(name, id, relname):
     item = R.query.get_or_404(id)
     q = {get_rel_defn(relname)["per"]: item.id}
     relvals = rel.query.filter_by(**q).all()
-    
-    return render_template("resource/rel/index.html", name=name, R=R, relname=relname, rel=rel,
-                           relvals=relvals, rel_defn=get_defn(relname, scope="summary"), item=item,
+
+    return render_template("resource/rel/index.html",
+                           name=name,
+                           R=R,
+                           relname=relname,
+                           rel=rel,
+                           relvals=relvals,
+                           rel_defn=get_defn(relname, scope="summary"),
+                           item=item,
                            defn=get_defn(name, scope="display"))
+
 
 @login_required
 @resource.route("/<name>/-/<id>/rel/<relname>/new", methods=["GET", "POST"])
 def relnew(name, id, relname):
-    pass
+    try:
+        R = get_enum(name)
+        rel = get_rel(relname)
+    except:
+        abort(403)
+
+    item = R.query.get_or_404(id)
+    q = {get_rel_defn(relname)["per"]: item.id}
+    relval = rel(**q)
+    item.title = ""
+
+    if request.method == "POST":
+        pass
+
+        return redirect(url_for('resource.index', name=name))
+
+    return render_template("resource/rel/new.html",
+                           name=name,
+                           R=R,
+                           relname=relname,
+                           rel=rel,
+                           relval=relval,
+                           rel_defn=get_defn(relname, scope="display"),
+                           item=item,
+                           defn=get_defn(name, scope="display"),
+                           defn_parse=defn_parse,
+                           render_column=render_column,
+                           defn_parse_raw=defn_parse_raw,
+                           get_enum=get_enum,
+                           get_rel_defn=get_rel_defn)
+
 
 @login_required
 @resource.route("/<name>/-/<id>/rel/<relname>/show/<relid>", methods=["GET", "POST"])
 def relshow(name, id, relname, relid):
     pass
 
+
 @login_required
 @resource.route("/<name>/-/<id>/rel/<relname>/edit/<relid>", methods=["GET", "POST"])
 def reledit(name, id, relname, relid):
     pass
 
+
 @login_required
 @resource.route("/<name>/-/<id>/rel/<relname>/delete/<relid>", methods=["GET", "POST"])
 def reldelete(name, id, relname, relid):
     pass
+
+def apply_defn_post_data_to_obj(defn, obj):
+    for slide in defn['slides']:
+        for part in slide['parts']:
+            if part["component"] == "standalone":
+                cols = [part['single']]
+            elif part["component"] == "table":
+                cols = part['columns']
+            else:
+                continue  # for now
+
+            for column in cols:
+                if column['type'] in ['input', 'text', 'reference', 'reference_list']:
+                    # These are the simple (singular) column types
+                    if column['column'] not in request.form.keys():
+                        continue
+
+                    if column['type'] in ['input', 'text']:
+                        setattr(obj, column['column'],
+                                request.form[column['column']])
+                    elif column['type'] == 'reference':
+                        cls = get_enum(column['refersto'])
+                        obj = cls.query.get(request.form[column['column']])
+                        setattr(obj, column['column'], obj)
+                    elif column['type'] == 'reference_list':
+                        cls = get_enum(column['refersto'])
+                        data = [cls.query.get(
+                            i) for i in request.form.getlist(column['column'])]
+                        setattr(obj, column['column'], data)
+                elif column['type'] in ['dimension']:
+                    # Special Dimension type
+                    for colname in column['column']:
+                        value = request.form[colname].strip()
+                        if value == '':
+                            value = None
+                        setattr(obj, colname, value)
+                else:
+                    print("LOG: unsaveable column %s" % column['column'])
+    return obj
