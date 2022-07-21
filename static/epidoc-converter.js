8af2c34c8482491a764392196f45bde38cba74cc
luap42 <pstrobach15+luap42@gmail.com>
2022-07-21 09:56:51 +0200

    Implement conversion

---
+++ static/epidoc-converter.js
@@ -0,0 +1,24 @@
+if(epidoc_btn = document.querySelector('[data-ext-hint=epidoc-btn]')) {
+    epidoc_target = document.querySelector('[data-ext-hint=epidoc-target]')
+    epidoc_source = document.querySelector('[data-ext-hint=epidoc-source]')
+
+    // We do not use JQuery, but we can probably mock it for the converter
+    function $(q) {
+        if (q == "#textImportStartingLine")
+            return {
+                val: () => 1
+            };
+        else if (q == "#textImportMode")
+            return {
+                val: () => "newText"
+            };
+        else if (q == "#langSource")
+            return {
+                val: () => "la" // for now, maybe make flexible?
+            };
+    }
+
+    epidoc_btn.addEventListener("click", () => {
+        epidoc_target.value = convertAncientText(epidoc_source.value, 'edr')
+    })
+}
