40aa46fecf635a8e9a18a5a7b2fe63230f143385
luap42 <pstrobach15+luap42@gmail.com>
2022-10-27 10:41:43 +0200

    Fix issue with .where vs .filter

--- controllers/search.py
+++ controllers/search.py
@@ -20,34 +20,34 @@ def basic_do():
     places = Places.query
 
     if 'mappola_id' in request.values.keys() and (mappola_id := request.values.get('mappola_id')) != '':
-        query = query.where(Inscriptions.id==mappola_id)
+        query = query.filter(Inscriptions.id==mappola_id)
     
     if 'province' in request.values.keys() and (province := request.values.get('province')) != '':
-        places = places.filter_by(province_id=province)
+        places = places.filter(Places.province_id==province)
 
     if 'region' in request.values.keys() and (region := request.values.get('region')) != '':
-        places = places.filter_by(modern_region_id=region)
+        places = places.filter(Places.modern_region_id==region)
 
     if 'state' in request.values.keys() and (state := request.values.get('state')) != '':
         regions_in_state = [i.id for i in ModernRegions.query.filter_by(state_id=state).all()]
-        places = places.where(Places.id.in_(regions_in_state))
+        places = places.filter(Places.id.in_(regions_in_state))
 
     if 'date_min' in request.values.keys() and (date_min := request.values.get('date_min')) != '':
-        query = query.where(Inscriptions.date_begin >= date_min)
+        query = query.filter(Inscriptions.date_begin >= date_min)
 
     if 'date_max' in request.values.keys() and (date_max := request.values.get('date_max')) != '':
-        query = query.where(Inscriptions.date_end <= date_max)
+        query = query.filter(Inscriptions.date_end <= date_max)
 
 
-    query = query.where(Inscriptions.place_id.in_([i[0] for i in places.values(Places.id)]))
+    query = query.filter(Inscriptions.place_id.in_([i[0] for i in places.values(Places.id)]))
 
     if 'text1' in request.values.keys() and (text1 := request.values.get('text1')) != '':
-        text_1_query = query.where(Inscriptions.text_epidoc_form.like(f"%{text1}%"))
+        text_1_query = query.filter(Inscriptions.text_epidoc_form.like(f"%{text1}%"))
     else:
         text_1_query = None
     
     if 'text2' in request.values.keys() and (text2 := request.values.get('text2')) != '':
-        text_2_query = query.where(Inscriptions.text_epidoc_form.like(f"%{text2}%"))
+        text_2_query = query.filter(Inscriptions.text_epidoc_form.like(f"%{text2}%"))
     else:
         text_2_query = None
 
@@ -55,17 +55,17 @@ def basic_do():
         'text2' in request.values.keys() and (text2 := request.values.get('text2')) != '':
         
         if request.values.get('text_conj', 'AND') == 'OR':
-            query = query.where(Inscriptions.text_epidoc_form.like(f"%{text1}%")) \
-                .union(query.where(Inscriptions.text_epidoc_form.like(f"%{text2}%")))
+            query = query.filter(Inscriptions.text_epidoc_form.like(f"%{text1}%")) \
+                .union(query.filter(Inscriptions.text_epidoc_form.like(f"%{text2}%")))
         else:
-            query = query.where(Inscriptions.text_epidoc_form.like(f"%{text1}%"))
-            query = query.where(Inscriptions.text_epidoc_form.like(f"%{text2}%"))
+            query = query.filter(Inscriptions.text_epidoc_form.like(f"%{text1}%"))
+            query = query.filter(Inscriptions.text_epidoc_form.like(f"%{text2}%"))
 
     elif text1:
-        query = query.where(Inscriptions.text_epidoc_form.like(f"%{text1}%"))
+        query = query.filter(Inscriptions.text_epidoc_form.like(f"%{text1}%"))
 
     elif text2:
-        query = query.where(Inscriptions.text_epidoc_form.like(f"%{text2}%"))
+        query = query.filter(Inscriptions.text_epidoc_form.like(f"%{text2}%"))
 
     count = query.count()
 
