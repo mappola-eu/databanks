fe410f4a17a70462e9663d4a56569142b2628d3f
luap42 <pstrobach15+luap42@gmail.com>
2022-12-01 21:27:06 +0100

    Add text layout tags

--- models/inscriptions.py
+++ models/inscriptions.py
@@ -2,7 +2,8 @@ from . import db
 from sqlalchemy import event
 from ..linkage import LINKERS
 from ..linkage.epidoc import *
-import json, re
+import json
+import re
 
 with open("models/definition.json", "r") as f:
     defn = json.load(f)
@@ -57,6 +58,17 @@ inscription_verse_type_assoc = db.Table(
         db.Integer(),
         db.ForeignKey('verse_timing_types.id')))
 
+inscription_text_layout_tag_assoc = db.Table(
+    'inscription_text_layout_tag_assoc',
+    db.Column(
+        'inscription_id',
+        db.Integer(),
+        db.ForeignKey('inscriptions.id')),
+    db.Column(
+        'text_layout_tag_id',
+        db.Integer(),
+        db.ForeignKey('text_layout_tags.id')))
+
 inscription_people_assoc = db.Table(
     'inscription_people_assoc',
     db.Column(
@@ -68,6 +80,7 @@ inscription_people_assoc = db.Table(
         db.Integer(),
         db.ForeignKey('people.id')))
 
+
 class Inscriptions(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(180))
@@ -75,22 +88,29 @@ class Inscriptions(db.Model):
 
     place_id = db.Column(db.Integer, db.ForeignKey('places.id'))
     find_comment = db.Column(db.Text)
-    current_location_id = db.Column(db.Integer, db.ForeignKey('current_locations.id'))
+    current_location_id = db.Column(
+        db.Integer, db.ForeignKey('current_locations.id'))
 
     object_type_id = db.Column(db.Integer, db.ForeignKey('object_types.id'))
-    object_material_id = db.Column(db.Integer, db.ForeignKey('object_materials.id'))
-    object_preservation_state_id = db.Column(db.Integer, db.ForeignKey('object_preservation_states.id'))
-    object_dim_height = db.Column(db.Float) # Y
-    object_dim_width = db.Column(db.Float) # X
-    object_dim_depth = db.Column(db.Float) # Z
-    object_execution_technique_id = db.Column(db.Integer, db.ForeignKey('object_execution_techniques.id'))
+    object_material_id = db.Column(
+        db.Integer, db.ForeignKey('object_materials.id'))
+    object_preservation_state_id = db.Column(
+        db.Integer, db.ForeignKey('object_preservation_states.id'))
+    object_dim_height = db.Column(db.Float)  # Y
+    object_dim_width = db.Column(db.Float)  # X
+    object_dim_depth = db.Column(db.Float)  # Z
+    object_execution_technique_id = db.Column(
+        db.Integer, db.ForeignKey('object_execution_techniques.id'))
     object_decoration_comment = db.Column(db.Text)
     object_text_layout_comment = db.Column(db.Text)
-    
+    object_text_layout_tags = db.relationship(
+        'TextLayoutTags', secondary=inscription_text_layout_tag_assoc, backref='inscriptions')
+
     letter_size_min = db.Column(db.Float)
     letter_size_max = db.Column(db.Float)
 
-    text_function_id = db.Column(db.Integer, db.ForeignKey('text_functions.id'))
+    text_function_id = db.Column(
+        db.Integer, db.ForeignKey('text_functions.id'))
     text_epidoc_form = db.Column(db.Text)
 
     text_interpretative_cached = db.Column(db.Text)
@@ -107,30 +127,39 @@ class Inscriptions(db.Model):
     religion_id = db.Column(db.Integer, db.ForeignKey('religions.id'))
 
     place = db.relationship('Places', backref='inscriptions')
-    current_location = db.relationship('CurrentLocations', backref='inscriptions')
+    current_location = db.relationship(
+        'CurrentLocations', backref='inscriptions')
     object_type = db.relationship('ObjectTypes', backref='inscriptions')
-    object_material = db.relationship('ObjectMaterials', backref='inscriptions')
-    object_preservation_state = db.relationship('ObjectPreservationStates', backref='inscriptions')
-    object_execution_technique = db.relationship('ObjectExecutionTechniques', backref='inscriptions')
+    object_material = db.relationship(
+        'ObjectMaterials', backref='inscriptions')
+    object_preservation_state = db.relationship(
+        'ObjectPreservationStates', backref='inscriptions')
+    object_execution_technique = db.relationship(
+        'ObjectExecutionTechniques', backref='inscriptions')
     text_function = db.relationship('TextFunctions', backref='inscriptions')
     religion = db.relationship('Religions', backref='inscriptions')
 
-    languages = db.relationship('Languages', secondary=inscription_language_assoc, backref='inscriptions')
-    decoration_tags = db.relationship('ObjectDecorationTags', secondary=inscription_decoration_tag_assoc, backref='inscriptions')
-    dating_criteria = db.relationship('DatingCriteria', secondary=inscription_dating_criterion_assoc, backref='inscriptions')
-    verse_types = db.relationship('VerseTypes', secondary=inscription_verse_type_assoc, backref='inscriptions')
-    people = db.relationship('People', secondary=inscription_people_assoc, backref='inscriptions')
+    languages = db.relationship(
+        'Languages', secondary=inscription_language_assoc, backref='inscriptions')
+    decoration_tags = db.relationship(
+        'ObjectDecorationTags', secondary=inscription_decoration_tag_assoc, backref='inscriptions')
+    dating_criteria = db.relationship(
+        'DatingCriteria', secondary=inscription_dating_criterion_assoc, backref='inscriptions')
+    verse_types = db.relationship(
+        'VerseTypes', secondary=inscription_verse_type_assoc, backref='inscriptions')
+    people = db.relationship(
+        'People', secondary=inscription_people_assoc, backref='inscriptions')
 
     translations = db.relationship('Translations', backref='inscription')
 
     have_squeeze = db.Column(db.Boolean)
 
-    last_updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+    last_updated_at = db.Column(
+        db.DateTime, default=db.func.now(), onupdate=db.func.now())
     last_updated_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     last_updated_by = db.relationship('User')
     work_status_id = db.Column(db.Integer, db.ForeignKey('work_status.id'))
     work_status = db.relationship('WorkStatus')
-
 
     def long_id(self):
         if self.id is not None:
@@ -148,10 +177,10 @@ class Inscriptions(db.Model):
             base = base.replace("= <br", "<br")
 
         return base
-    
+
     def text_interpretative(self):
         return self.text_interpretative_cached.split('~~~APP BELOW~~~')[0]
-    
+
     def text_with_metrics_visualised(self):
         return self.text_metrics_visualised_cached
 
@@ -164,7 +193,7 @@ class Inscriptions(db.Model):
         if len(itt) == 1:
             return ""
 
-        return itt[1] 
+        return itt[1]
 
     def update_str(self):
         if self.last_updated_at is None:
@@ -175,37 +204,45 @@ class Inscriptions(db.Model):
         else:
             return self.last_updated_at.strftime("%Y-%m-%d") + ", by " + self.last_updated_by.full_name
 
+
 class WorkStatus(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(100))
     enum_lod = db.Column(db.String(150))
+
 
 class ObjectTypes(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(100))
     enum_lod = db.Column(db.String(150))
 
+
 class ObjectMaterials(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(100))
     enum_lod = db.Column(db.String(150))
+
 
 class ObjectPreservationStates(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(100))
     enum_lod = db.Column(db.String(150))
 
+
 class ObjectExecutionTechniques(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(100))
     enum_lod = db.Column(db.String(150))
 
+
 class ObjectDecorationTags(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(100))
 
-    parent_object_decoration_tag_id = db.Column(db.Integer, db.ForeignKey('object_decoration_tags.id'))
-    parent_object_decoration_tag = db.relationship('ObjectDecorationTags', remote_side=[id])
+    parent_object_decoration_tag_id = db.Column(
+        db.Integer, db.ForeignKey('object_decoration_tags.id'))
+    parent_object_decoration_tag = db.relationship(
+        'ObjectDecorationTags', remote_side=[id])
     enum_lod = db.Column(db.String(150))
 
     def parent_object_decoration_tag_name(self):
@@ -213,27 +250,32 @@ class ObjectDecorationTags(db.Model):
             return self.parent_object_decoration_tag.title
         else:
             return self.title
-    
+
+
 class TextFunctions(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(100))
     enum_lod = db.Column(db.String(150))
-    
+
+
 class VerseTimingTypes(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(100))
     enum_lod = db.Column(db.String(150))
-    
+
+
 class Languages(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(100))
     enum_lod = db.Column(db.String(150))
-    
+
+
 class CurrentLocations(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(100))
     enum_lod = db.Column(db.String(150))
-    
+
+
 class Places(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(100))
@@ -246,7 +288,8 @@ class Places(db.Model):
 
     province_id = db.Column(db.Integer, db.ForeignKey('provinces.id'))
     province = db.relationship('Provinces', backref='places')
-    modern_region_id = db.Column(db.Integer, db.ForeignKey('modern_regions.id'))
+    modern_region_id = db.Column(
+        db.Integer, db.ForeignKey('modern_regions.id'))
     modern_region = db.relationship('ModernRegions', backref='places')
 
     pleiades_id = db.Column(db.Integer())
@@ -258,11 +301,13 @@ class Places(db.Model):
             return '-'
 
         return f"{self.modern_region.title} ({self.modern_region.modern_state_name()})"
-    
+
+
 class Provinces(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(100))
     enum_lod = db.Column(db.String(150))
+
 
 class ModernRegions(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
@@ -275,60 +320,72 @@ class ModernRegions(db.Model):
     def modern_state_name(self):
         return self.state.title if self.state else '-'
 
+
 class ModernStates(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(100))
     enum_lod = db.Column(db.String(150))
-    
+
+
 class PeopleGenders(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(100))
     enum_lod = db.Column(db.String(150))
-    
+
+
 class PeopleAges(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(100))
     enum_lod = db.Column(db.String(150))
-    
+
+
 class PeopleAgeExpressions(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(100))
     enum_lod = db.Column(db.String(150))
 
+
 class PeopleAgePrecision(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(100))
     enum_lod = db.Column(db.String(150))
-    
+
+
 class PeopleOrigins(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(100))
     enum_lod = db.Column(db.String(150))
-    
+
+
 class PeopleLegalStatus(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(100))
     enum_lod = db.Column(db.String(150))
-    
+
+
 class PeopleRanks(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(100))
     enum_lod = db.Column(db.String(150))
-    
+
+
 class PeopleProfessions(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(100))
     enum_lod = db.Column(db.String(150))
-    
+
+
 class DatingCriteria(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(100))
     enum_lod = db.Column(db.String(150))
-    
+
+
 class VerseTypes(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(100))
-    parent_verse_type_id = db.Column(db.Integer, db.ForeignKey('verse_types.id'))
+    parent_verse_type_id = db.Column(
+        db.Integer, db.ForeignKey('verse_types.id'))
     parent_verse_type = db.relationship('VerseTypes', remote_side=[id])
     enum_lod = db.Column(db.String(150))
 
@@ -337,6 +394,7 @@ class VerseTypes(db.Model):
             return self.parent_verse_type.title
         else:
             return self.title
+
 
 class Translations(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
@@ -356,6 +414,13 @@ class Translations(db.Model):
         else:
             return self.translated_form
 
+
+class TextLayoutTags(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(100))
+    enum_lod = db.Column(db.String(150))
+
+
 class Publications(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     inscription_id = db.Column(db.Integer, db.ForeignKey('inscriptions.id'))
@@ -367,18 +432,25 @@ class Publications(db.Model):
     def display(self):
         return self.reference_comment + " (" + self.zotero_item_id + ")"
 
+
 class People(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     reference_link = db.Column(db.Text)
     name = db.Column(db.Text)
-    people_gender_id = db.Column(db.Integer, db.ForeignKey('people_genders.id'))
+    people_gender_id = db.Column(
+        db.Integer, db.ForeignKey('people_genders.id'))
     people_age_id = db.Column(db.Integer, db.ForeignKey('people_ages.id'))
-    people_age_expression_id = db.Column(db.Integer, db.ForeignKey('people_age_expressions.id'))
-    people_age_precision_id = db.Column(db.Integer, db.ForeignKey('people_age_precision.id'))
-    people_origin_id = db.Column(db.Integer, db.ForeignKey('people_origins.id'))
-    people_legal_status_id = db.Column(db.Integer, db.ForeignKey('people_legal_status.id'))
+    people_age_expression_id = db.Column(
+        db.Integer, db.ForeignKey('people_age_expressions.id'))
+    people_age_precision_id = db.Column(
+        db.Integer, db.ForeignKey('people_age_precision.id'))
+    people_origin_id = db.Column(
+        db.Integer, db.ForeignKey('people_origins.id'))
+    people_legal_status_id = db.Column(
+        db.Integer, db.ForeignKey('people_legal_status.id'))
     people_rank_id = db.Column(db.Integer, db.ForeignKey('people_ranks.id'))
-    people_profession_id = db.Column(db.Integer, db.ForeignKey('people_professions.id'))
+    people_profession_id = db.Column(
+        db.Integer, db.ForeignKey('people_professions.id'))
 
     gender = db.relationship('PeopleGenders', backref='people')
     age = db.relationship('PeopleAges', backref='people')
@@ -389,24 +461,28 @@ class People(db.Model):
     rank = db.relationship('PeopleRanks', backref='people')
     profession = db.relationship('PeopleProfessions', backref='people')
 
+
 class Religions(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(100))
     enum_lod = db.Column(db.String(150))
 
+
 def get_enum(enum):
     if enum in defn["classes"]:
         return eval(enum)
-    
+
     else:
         raise NameError("Enumeration not found: ", enum)
+
 
 def get_rel(rel):
     if rel in defn["relations"].keys():
         return eval(rel)
-    
+
     else:
         raise NameError("Relation not found: ", rel)
+
 
 def get_defn(enum, scope="display"):
     if enum in defn["classes"] or enum in defn["relations"].keys():
@@ -422,12 +498,14 @@ def get_defn(enum, scope="display"):
     else:
         raise NameError("Enumeration not found: ", enum)
 
+
 def get_rel_defn(rel):
     if rel in defn["relations"].keys():
         return defn["relations"][rel]
-    
+
     else:
         raise NameError("Relation not found: ", rel)
+
 
 def defn_parse_raw(code, item, **args):
     on, key = code.split("#")
@@ -440,8 +518,8 @@ def defn_parse_raw(code, item, **args):
 
     if key == "":
         return on_obj
-    
-    exec_func =  key.endswith("()")
+
+    exec_func = key.endswith("()")
     if exec_func:
         key = key[:-2]
 
@@ -453,8 +531,10 @@ def defn_parse_raw(code, item, **args):
     else:
         return ""
 
+
 def defn_parse(*args, **kwargs):
     return str(defn_parse_raw(*args, **kwargs))
+
 
 def render_column(item, col):
     linkage = None
@@ -475,7 +555,7 @@ def render_column(item, col):
         return (defn_parse(col["column"], item), linkage)
     elif col["type"] == "dimension":
         width, height, depth = getattr(item, col["column"][0]), getattr(item, col["column"][1]), \
-                                   getattr(item, col["column"][2])
+            getattr(item, col["column"][2])
         return ([width, height, depth], linkage)
     elif col["type"] == "reference":
         if hasattr(item, col["column"]) and (column_value := getattr(item, col["column"])) is not None:
@@ -491,10 +571,12 @@ def render_column(item, col):
             return [(getattr(i, col["render"])(), linker.link(i)) for i in getattr(item, col["column"])]
     return ("", linkage)
 
+
 def defn_snippet(snippet, *args, **kwargs):
     return re.sub(r"\$\{([a-zA-Z]+\#[a-zA-Z]+(?:\(\))?)\}",
                   lambda mo: defn_parse(mo.group(1), *args, **kwargs),
                   snippet)
+
 
 def get_enum_with_grouping(refersto, grouping):
     data = get_enum(refersto).query.all()
@@ -511,20 +593,21 @@ def get_enum_with_grouping(refersto, grouping):
             groups[group] += [e]
         else:
             groups[group] = [e]
-        
+
     out_data = []
     for gv in groups.values():
         out_data += [{
             "label": defn_parse_raw(grouping['representation'], gv[0]),
             "entities": gv
         }]
-    
+
     return out_data
+
 
 def postproc(data, type_):
     if 'post_process' not in type_:
         return data
-    
+
     elif type_['post_process'] == 'date':
         if not data[0]:
             return '', data[1]
