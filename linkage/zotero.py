c0b60dabf603ca583ca70d06bdccf62b3f3412a0
luap42 <pstrobach15+luap42@gmail.com>
2022-12-21 15:10:31 +0100

    smarter zotero linkage

--- linkage/zotero.py
+++ linkage/zotero.py
@@ -9,7 +9,11 @@ class ZoteroLinker:
     def link(self, item):
         item_id = item.zotero_item_id
 
-        link_format = "https://www.zotero.org/groups/" + self.zotero_id + \
-            "/-/items/" + item_id + "/item-details"
+        if item_id:
+            link_format = "https://www.zotero.org/groups/" + self.zotero_id + \
+                "/-/items/" + item_id + "/item-details"
 
-        return link_format
+            return link_format
+        
+        elif item.reference_comment.startswith("http:") or item.reference_comment.startswith("https:") or item.reference_comment.startswith("ftp:"):
+            return item.reference_comment
