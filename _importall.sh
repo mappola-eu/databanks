a576597da47789a215fbab33ddb886c1d30c7200
luap42 <pstrobach15+luap42@gmail.com>
2023-05-04 10:22:06 +0200

    Progress on import tool

--- _importall.sh
+++ _importall.sh
@@ -8,4 +8,4 @@ flask import enum Provinces import_data/Provinces.csv
 flask import enum ObjectPreservationStates import_data/StateOfPreservation.csv
 flask import enum VerseTimingTypes import_data/VerseTimingTypes.csv
 
-flask import complex ObjectDecorationTags import_data/ObjectDecorationTags.c.json
+flask import enum:complex ObjectDecorationTags import_data/ObjectDecorationTags.c.json
--- imports.py
+++ imports.py
@@ -1,4 +1,5 @@
 import csv, json
+import xml.etree.ElementTree as ET
 import click
 from flask import Blueprint
 from .models import db, get_enum
@@ -33,10 +34,10 @@ def enum(enum_name, from_file):
     db.session.commit()
     print("IMPORT END, Success.")
 
-@import_.cli.command("complex")
+@import_.cli.command("enum:complex")
 @click.argument("enum_name")
 @click.argument("from_file")
-def complex(enum_name, from_file):
+def enum_complex(enum_name, from_file):
     print("IMPORT BEGIN:", enum_name)
     enum = get_enum(enum_name)
     with open(from_file, "r") as complex_data_file:
@@ -86,8 +87,41 @@ def rerender_insc_text():
 
     for i in insc.query.all():
         full_parse_on_inscription(i)
-        if inscription.text_interpretative_cached == "EPIDOC IS INVALID; UPDATE WITH WELL-FORMED XML":
+        if i.text_interpretative_cached == "EPIDOC IS INVALID; UPDATE WITH WELL-FORMED XML":
             print(f"Inscription #{i.id} failed to render.")
     
     db.session.commit()
     print("Done.")
+
+@import_.cli.command("inscription")
+@click.argument("from_file")
+def inscription(from_file):
+    with open("models/xmlmapping.txt", "r") as xmlmapfile:
+        xmlmapraw = xmlmapfile.read()
+    
+    tree = ET.parse(from_file)
+    root = tree.getroot()
+    
+    xmlmap = {}
+    for line in xmlmapraw.split("\n"):
+        col, expr = line.split(":", 1)
+        col, expr = col.strip(), expr.strip()
+
+        mod = None
+        path = expr
+
+        if "->" in expr:
+            path, mod = expr.rsplit("->", 1)
+            path, mod = path.strip(), mod.strip()
+        
+        xmlmap[col] = (path, mod)
+    
+    outmap = {}
+
+    print(root.find("./*", namespaces={'tei': 'http://www.tei-c.org/ns/1.0'}))
+    return
+
+    for col, (path, mod) in xmlmap.items():
+        node = root.find("." + path)
+        print(path)
+        print(node)
