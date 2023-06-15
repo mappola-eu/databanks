b3d4106b1f771ac264caa16077a2cd6dee7fdafe
luap42 <pstrobach15+luap42@gmail.com>
2023-06-15 10:49:43 +0200

    Much progress on import script

--- imports.py
+++ imports.py
@@ -2,8 +2,9 @@ import csv, json
 import xml.etree.ElementTree as ET
 import click
 from flask import Blueprint
-from .models import db, get_enum
+from .models import db, get_enum, get_defn, Inscriptions
 from .linkage.epidoc import full_parse_on_inscription
+from .controllers.resource import apply_defn_post_data_to_obj
 
 import_ = Blueprint('import', __name__)
 
@@ -99,8 +100,10 @@ def inscription(from_file):
     with open("models/xmlmapping.txt", "r") as xmlmapfile:
         xmlmapraw = xmlmapfile.read()
     
-    tree = ET.parse(from_file)
-    root = tree.getroot()
+    tree = ET.iterparse(from_file)
+    for _, el in tree:
+        el.tag = el.tag.split('}', 1)[1]  # strip all namespaces
+    root = tree.root
     
     xmlmap = {}
     for line in xmlmapraw.split("\n"):
@@ -117,11 +120,98 @@ def inscription(from_file):
         xmlmap[col] = (path, mod)
     
     outmap = {}
-
-    print(root.find("./*", namespaces={'tei': 'http://www.tei-c.org/ns/1.0'}))
-    return
+    conflicts = []
 
     for col, (path, mod) in xmlmap.items():
         node = root.find("." + path)
-        print(path)
-        print(node)
+        value = import_mods(col, mod, node, conflicts)
+
+        outmap[col] = value
+
+    print("Conflicts found:\n---\n\nCannot associate the following values")
+    for c in conflicts:
+            print(f"{c[0]:24} = ({c[1]}) {c[2]}")
+    
+    print()
+
+    if len(conflicts) >= 4:
+        print("ATTENTION! There are more than four conflicts.")
+
+        if input("Continue? (Yn) ").lower() == 'n':
+            return
+
+    i = Inscriptions()
+
+    for col, val in outmap.items():
+        if not val: continue
+        print(col, repr(val))
+        setattr(i, col, val)
+
+    defn = get_defn('Inscriptions')
+    apply_defn_post_data_to_obj(defn, i, True)
+
+    print(i)
+    
+    #db.session.add(i)
+    #db.session.commit()
+
+
+def import_mods(col, mod, node, conflicts):
+    if mod == 'text':
+        return node.text
+    
+    elif mod == 'minmaxmin':
+        return float(node.text.split("-")[0].replace(",", "."))
+    
+    elif mod == 'minmaxmax':
+        return float(node.text.split("-")[-1].replace(",", "."))
+    
+    elif mod == "outerXML":
+        return ET.tostring(node, encoding="utf-8").decode("utf-8")
+    
+    elif mod == "stripXML":
+        stripXML = lambda tag: (tag.text or '') + ''.join(stripXML(e) for e in tag) + (tag.tail or '')
+
+        return stripXML(node).strip().replace("<br>", "\n")
+    
+    elif mod.startswith("ref:") or mod.startswith("ref.lod:"):
+        if mod.startswith("ref:"):
+            ref = mod[len("ref:"):]
+            value = node.text
+            col = 'title'
+            if ":" in ref:
+                ref, col = ref.split(":")
+
+        elif mod.startswith("ref.lod:"):
+            ref = mod[len("ref.lod:"):]
+            col = 'enum_lod'
+            value = node.attrib['ref']
+
+            if value.startswith("http:"):
+                value = "https:" + value[len("http:"):]
+
+        R = get_enum(ref)
+
+        # Check for exact match
+        match = R.query.filter_by(**{col: value}).all()
+        if len(match) == 1:
+            return match[0]
+
+        # Check for rough match
+        match = R.query.filter(getattr(R, col).like(f"%{value}%")).all()
+        if len(match) == 1:
+            return match[0]
+
+        # Check for loose match
+        matcher = '%' + '%'.join(value) + '%'
+        match = R.query.filter(getattr(R, col).like(matcher)).all()
+        if len(match) == 1:
+            return match[0]
+        
+        conflicts.append([ref, col, value])
+    
+    elif mod.startswith("attr:@"):
+        attr = mod[len("attr:@"):]
+        return node.attrib[attr]
+    
+    return None
