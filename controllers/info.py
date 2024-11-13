e6c9eeec04fddec8ced5004e75c834efc3ad3399
luap42 <pstrobach15+luap42@gmail.com>
2024-11-13 17:08:14 +0100

    Further info pages

--- controllers/info.py
+++ controllers/info.py
@@ -4,5 +4,16 @@ from ..models import db, get_enum
 info = Blueprint('info', __name__)
 
 @info.route("/project/concept-and-aim")
-def concept_and_aim():
-    return render_template("info/project/concept_and_aim.html")
+def concept_and_aim(): return render_template("info/project/concept_and_aim.html")
+
+@info.route("/project/team")
+def team(): return render_template("info/project/team.html")
+
+@info.route("/project/partners")
+def partners(): return render_template("info/project/partners.html")
+
+@info.route("/project/events-and-presentations")
+def events_and_presentations(): return render_template("info/project/events_and_presentations.html")
+
+@info.route("/project/acknowledgements")
+def acknowledgements(): return render_template("info/project/acknowledgements.html")
