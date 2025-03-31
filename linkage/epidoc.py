202d8c863b63ebdac67df27a50d8ca2decbab0ed
luap42 <pstrobach15+luap42@gmail.com>
2025-03-31 14:24:00 +0200

    Increase timeout for epidoc conversion

--- linkage/epidoc.py
+++ linkage/epidoc.py
@@ -44,7 +44,7 @@ def full_parse_on_inscription(inscription):
     encoded = b64encode(epidoc.encode())
 
     pipe = Popen(['python3', __file__], stdin=PIPE, stdout=PIPE)
-    response = pipe.communicate(encoded + b'\n', timeout=4)
+    response = pipe.communicate(encoded + b'\n', timeout=6)
 
     if not len(response[0]):
         inscription.text_interpretative_cached = "EPIDOC IS INVALID; UPDATE WITH WELL-FORMED XML"
