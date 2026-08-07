ee0143562ecb085aca549d6781b6028e6e5c854d
luap42 <pstrobach15+luap42@gmail.com>
2026-08-07 12:28:43 +0200

    Download all inscriptions tool w/ captcha

--- controllers/inscriptions.py
+++ controllers/inscriptions.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template, make_response, abort, send_file
-from ..models import Inscriptions, inscriptions_to_json, inscriptions_to_csv
+from flask import Blueprint, render_template, make_response, abort, send_file, request, flash, Response
+from ..models import Inscriptions, WorkStatus, inscriptions_to_json, inscriptions_to_csv
 from flask_security import login_required
 
-import csv, io
+import csv, io, random, zipfile
 
 inscriptions = Blueprint('inscriptions', __name__)
 
@@ -31,6 +31,30 @@ def render_xml(id):
     return response
 
 
+@inscriptions.route("/download-xml", methods=["GET", "POST"])
+def download_xml():
+    if request.method == 'POST':
+        captcha = Inscriptions.query.get_or_404(request.form['cid'])
+
+        if captcha.title.strip() == request.form['ctitle'].strip():
+            zip_file = _generate_zipfile()
+
+            return Response(zip_file, mimetype='application/zip', headers={
+                'Content-Disposition': f'attachment;filename=MAPPOLADATA.zip'
+            })
+
+
+        else:
+            flash("Error: The captcha input was invalid.")
+
+    acceptable_work_status = WorkStatus.query.filter_by(is_complete=True).first()
+    captcha_insc_query = Inscriptions.query.filter_by(work_status=acceptable_work_status)
+    max_offset = captcha_insc_query.count()
+    captcha_insc = captcha_insc_query.offset(random.randint(0, max_offset - 1)).first()
+
+    return render_template("inscriptions/download_xml.html", captcha_insc=captcha_insc)
+
+
 @login_required
 @inscriptions.route("/.unknown/alex.csv")
 def alex_export():
@@ -48,3 +72,19 @@ def alex_export():
     mem_io.seek(0)
 
     return send_file(mem_io, mimetype='text/csv')
+
+
+def _generate_zipfile():
+    zip_io = io.BytesIO()
+    zip_file = zipfile.ZipFile(zip_io, mode='w')
+
+    for insc in Inscriptions.query.all():
+        if insc.work_status and insc.work_status.is_deleted:
+            continue
+
+        xml_file = render_template("inscriptions/one.xml", insc=insc)
+        zip_file.writestr(f"{insc.long_id()}.xml", xml_file)
+    
+    zip_file.close()
+
+    return zip_io.getvalue()
